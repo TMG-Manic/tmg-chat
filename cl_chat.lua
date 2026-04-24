24004e54c65e9f7e28bcccf0d76f444db4e60895
TMG_Manic <tmgfakeacc5@outlook.com>
2026-04-23 20:29:59 -0400

    Update cl_chat.lua

--- cl_chat.lua
+++ cl_chat.lua
@@ -1,14 +1,31 @@
 local chatInputActive = false
 local chatInputActivating = false
 local chatLoaded = false
-local chatHidden = false
 local currentResourceName = GetCurrentResourceName()
-local isRDR = GetGameName() == 'rdr3'
 
+-- Visibility States
+local CHAT_HIDE_STATES = {
+    SHOW_WHEN_ACTIVE = 0,
+    ALWAYS_SHOW = 1,
+    ALWAYS_HIDE = 2
+}
+
+local kvpEntry = GetResourceKvpString('hideState')
+local chatHideState = kvpEntry and tonumber(kvpEntry) or CHAT_HIDE_STATES.SHOW_WHEN_ACTIVE
+local isFirstHide = true
+local lastChatHideState = -1
+local origChatHideState = -1
+
+-- ==========================================
+--  Security & Utility
+-- ==========================================
 local function UsePreSecurityBehavior()
     return GetConvar('sysresource_chat_disableOriginSecurityChecks', 'true') == 'true'
 end
 
+-- ==========================================
+--  Optimization: RegisterKeyMapping (0.00ms)
+-- ==========================================
 RegisterCommand('chat_open', function()
     if not chatInputActive and not IsPauseMenuActive() then
         chatInputActive = true
@@ -19,9 +36,30 @@ end, false)
 
 RegisterKeyMapping('chat_open', 'Open Chat', 'keyboard', 'T')
 
+-- ==========================================
+--  Net Event Registration
+-- ==========================================
+RegisterNetEvent('chat:addMessage')
+RegisterNetEvent('chat:addTemplate')
+RegisterNetEvent('chat:addSuggestion')
+RegisterNetEvent('chat:addSuggestions')
+RegisterNetEvent('chat:removeSuggestion')
+RegisterNetEvent('chat:clear')
 RegisterNetEvent('__cfx_internal:serverPrint')
+
+-- ==========================================
+--  Event Handlers
+-- ==========================================
+
+AddEventHandler('chat:addMessage', function(message)
+    if type(message) == 'string' then message = { args = { message } } end
+    -- F8 Console Mirror
+    if message.args then print(table.concat(message.args, " ")) end
+    SendNUIMessage({ type = 'ON_MESSAGE', message = message })
+end)
+
 AddEventHandler('__cfx_internal:serverPrint', function(msg)
-    print(msg) -- Print to F8 console
+    print(msg) 
     SendNUIMessage({
         type = 'ON_MESSAGE',
         message = {
@@ -31,27 +69,6 @@ AddEventHandler('__cfx_internal:serverPrint', function(msg)
             mode = '_global'
         }
     })
-end)
-
-local addMessage = function(message)
-    if type(message) == 'string' then message = { args = { message } } end
-    SendNUIMessage({ type = 'ON_MESSAGE', message = message })
-end
-exports('addMessage', addMessage)
-AddEventHandler('chat:addMessage', addMessage)
-
-RegisterNetEvent('chatMessage')
-AddEventHandler('chatMessage', function(author, color, text)
-    local args = { text }
-    if author ~= "" then table.insert(args, 1, author) end
-    SendNUIMessage({
-        type = 'ON_MESSAGE',
-        message = { color = color, multiline = true, args = args }
-    })
-end)
-
-AddEventHandler('chat:addTemplate', function(id, html)
-    SendNUIMessage({ type = 'ON_TEMPLATE_ADD', template = { id = id, html = html } })
 end)
 
 AddEventHandler('chat:addSuggestion', function(name, help, params)
@@ -70,51 +87,32 @@ AddEventHandler('chat:clear', function()
     SendNUIMessage({ type = 'ON_CLEAR' })
 end)
 
-local function refreshCommands()
-    if GetRegisteredCommands then
-        local registeredCommands = GetRegisteredCommands()
-        local suggestions = {}
-        for _, command in ipairs(registeredCommands) do
-            if IsAceAllowed(('command.%s'):format(command.name)) then
-                table.insert(suggestions, { name = '/' .. command.name, help = '' })
-            end
-        end
-        TriggerEvent('chat:addSuggestions', suggestions)
+-- ==========================================
+--  Toggle Chat Logic (KVP Support)
+-- ==========================================
+RegisterCommand('toggleChat', function(source, args)
+    if not args[1] then
+        if chatHideState == CHAT_HIDE_STATES.SHOW_WHEN_ACTIVE then chatHideState = CHAT_HIDE_STATES.ALWAYS_SHOW
+        elseif chatHideState == CHAT_HIDE_STATES.ALWAYS_SHOW then chatHideState = CHAT_HIDE_STATES.ALWAYS_HIDE
+        elseif chatHideState == CHAT_HIDE_STATES.ALWAYS_HIDE then chatHideState = CHAT_HIDE_STATES.SHOW_WHEN_ACTIVE end
+    else
+        if args[1] == "visible" then chatHideState = CHAT_HIDE_STATES.ALWAYS_SHOW
+        elseif args[1] == "hidden" then chatHideState = CHAT_HIDE_STATES.ALWAYS_HIDE
+        elseif args[1] == "whenactive" then chatHideState = CHAT_HIDE_STATES.SHOW_WHEN_ACTIVE end
     end
-end
+    SetResourceKvp('hideState', tostring(chatHideState))
+end, false)
 
-local function refreshThemes()
-    local themes = {}
-    for resIdx = 0, GetNumResources() - 1 do
-        local resource = GetResourceByFindIndex(resIdx)
-        if GetResourceState(resource) == 'started' then
-            local themeName = GetResourceMetadata(resource, 'chat_theme')
-            if themeName then
-                local themeData = json.decode(GetResourceMetadata(resource, 'chat_theme_extra') or 'null')
-                if themeData then
-                    themeData.baseUrl = 'nui://' .. resource .. '/'
-                    themes[themeName] = themeData
-                end
-            end
-        end
-    end
-    SendNUIMessage({ type = 'ON_UPDATE_THEMES', themes = themes })
-end
-
+-- ==========================================
+--  NUI Callbacks
+-- ==========================================
 RegisterRawNuiCallback('chatResult', function(requestData, cb)
-    local resource = requestData.resource
-    local securityDisabled = UsePreSecurityBehavior()
-    if resource == nil and not securityDisabled then return end
-    
     chatInputActive = false
     SetNuiFocus(false, false)
-    
     local data = json.decode(requestData.body)
     if not data.canceled and data.message then
         if data.message:sub(1, 1) == '/' then
-            if resource == currentResourceName or securityDisabled then
-                ExecuteCommand(data.message:sub(2))
-            end
+            ExecuteCommand(data.message:sub(2))
         else
             TriggerServerEvent('_chat:messageEntered', GetPlayerName(PlayerId()), { 0, 153, 255 }, data.message, data.mode)
         end
@@ -124,18 +122,19 @@ end)
 
 RegisterNUICallback('loaded', function(data, cb)
     TriggerServerEvent('chat:init')
-    refreshCommands()
-    refreshThemes()
     chatLoaded = true
     cb('ok')
 end)
 
+-- ==========================================
+--  Optimized Background Thread (State Management)
+-- ==========================================
 Citizen.CreateThread(function()
     SetTextChatEnabled(false)
     SetNuiFocus(false, false)
 
     while true do
-        local sleep = 250 -- Check states every 250ms (0.00ms Resmon)
+        local sleep = 250 
 
         if chatInputActivating then
             SetNuiFocus(true, true)
@@ -144,23 +143,34 @@ Citizen.CreateThread(function()
         end
 
         if chatLoaded then
-            local shouldHide = IsScreenFadedOut() or IsPauseMenuActive()
-            if shouldHide ~= chatHidden then
-                chatHidden = shouldHide
+            local forceHide = IsScreenFadedOut() or IsPauseMenuActive()
+            local wasForceHide = false
+
+            -- Handle temporary force hides (fades/pause)
+            if chatHideState ~= CHAT_HIDE_STATES.ALWAYS_HIDE then
+                if forceHide then
+                    origChatHideState = chatHideState
+                    chatHideState = CHAT_HIDE_STATES.ALWAYS_HIDE
+                end
+            elseif not forceHide and origChatHideState ~= -1 then
+                chatHideState = origChatHideState
+                origChatHideState = -1
+                wasForceHide = true
+            end
+
+            -- Update NUI only if state actually changed
+            if chatHideState ~= lastChatHideState then
+                lastChatHideState = chatHideState
                 SendNUIMessage({
                     type = 'ON_SCREEN_STATE_CHANGE',
-                    shouldHide = chatHidden
+                    hideState = chatHideState,
+                    fromUserInteraction = not forceHide and not isFirstHide and not wasForceHide
                 })
+                isFirstHide = false
             end
         end
 
         if chatInputActive then sleep = 0 end
         Wait(sleep)
     end
-end)
-
-AddEventHandler('onClientResourceStart', function(resName)
-    Wait(500)
-    refreshCommands()
-    refreshThemes()
 end)
